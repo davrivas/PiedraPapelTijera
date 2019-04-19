16de2eb8310a76f12d162e1a07b53497eb3abe5f
David Rivas <d.rivas95@hotmail.com>
2019-04-19 18:54:34 -0500

    Adición de objetos Piedra, Papel y Tijera en la lista Opciones. Adición de jugadores individuales (Humano y Maquina). Captura de comando con parametro de tipo object para la opcion. Captura de la opción aleatoria de la máquina y comprobación de victoria, derrota o empate

--- PiedraPapelTijera/PiedraPapelTijera/VistaModelo/InicioVistaModelo.cs
+++ PiedraPapelTijera/PiedraPapelTijera/VistaModelo/InicioVistaModelo.cs
@@ -1,36 +1,117 @@
 ﻿using PiedraPapelTijera.Modelo;
-using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace PiedraPapelTijera.VistaModelo
 {
     public class InicioVistaModelo
     {
-        public ObservableCollection<Opcion> Opciones { get; private set; }
+        public Opcion Piedra { get; private set; }
+        public Opcion Papel { get; private set; }
+        public Opcion Tijera { get; private set; }
+        public IList<Opcion> Opciones { get; private set; }
+
         public Jugador Humano { get; private set; }
         public Jugador Maquina { get; private set; }
 
+        public ICommand ComandoEscogerOpcion { get; private set; }
+
+        public bool YaEscogio { get; private set; }
+
         public InicioVistaModelo()
         {
-            Opciones = new ObservableCollection<Opcion>
+            Piedra = new Opcion
             {
-                new Opcion
-                {
-                    Nombre = "Piedra",
-                    NombreArchivo = "piedra.png"
-                },
-                new Opcion
-                {
-                    Nombre = "Papel",
-                    NombreArchivo = "papel.png"
-                },
-                new Opcion
-                {
-                    Nombre = "Tijera",
-                    NombreArchivo = "tijera.png"
-                }
+                Nombre = "Piedra",
+                NombreArchivo = "piedra.png"
             };
+            Papel = new Opcion
+            {
+                Nombre = "Papel",
+                NombreArchivo = "papel.png"
+            };
+            Tijera = new Opcion
+            {
+                Nombre = "Tijera",
+                NombreArchivo = "tijera.png"
+            };
+            Opciones = new List<Opcion> { Piedra, Papel, Tijera };
+
             Humano = new Jugador { EsHumano = true };
             Maquina = new Jugador { EsHumano = false };
+
+            ComandoEscogerOpcion = new Command<object>(EscogerOpcion);
+
+            YaEscogio = Humano.Opcion != null;
+        }
+
+        private void EscogerOpcion(object obj)
+        {
+            var opcion = obj as Opcion;
+            Humano.Opcion = opcion;
+            YaEscogio = true;
+
+            MaquinaEscoge();
+        }
+
+        private void MaquinaEscoge()
+        {
+            var random = new Random();
+            int indiceOpcion = random.Next(Opciones.Count);
+            Maquina.Opcion = Opciones[indiceOpcion];
+
+            ComprobarOpcion();
+        }
+
+        private void ComprobarOpcion()
+        {
+            if (Humano.Opcion.Equals(Piedra))
+            {
+                if (Maquina.Opcion.Equals(Papel))
+                {
+                    // perdió
+                }
+                else if (Maquina.Opcion.Equals(Tijera))
+                {
+                    // ganó
+                }
+                else
+                {
+                    // empató
+                }
+            }
+            else if (Humano.Opcion.Equals(Papel))
+            {
+                if (Maquina.Opcion.Equals(Piedra))
+                {
+                    // ganó
+                }
+                else if (Maquina.Opcion.Equals(Tijera))
+                {
+                    // perdió
+                }
+                else
+                {
+                    // empató
+                }
+            }
+            else
+            {
+                if (Maquina.Opcion.Equals(Piedra))
+                {
+                    // perdió
+                }
+                else if (Maquina.Opcion.Equals(Papel))
+                {
+                    // ganó
+                }
+                else
+                {
+                    // empató
+                }
+            }
         }
     }
 }
