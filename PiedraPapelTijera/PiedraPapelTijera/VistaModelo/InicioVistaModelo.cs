ce38068739643d6005f7f5ef21791058eba09cb3
David Rivas <d.rivas95@hotmail.com>
2019-04-19 18:20:19 -0500

    Agregada lógica de InicioVistaModelo

--- PiedraPapelTijera/PiedraPapelTijera/VistaModelo/InicioVistaModelo.cs
+++ PiedraPapelTijera/PiedraPapelTijera/VistaModelo/InicioVistaModelo.cs
@@ -1,6 +1,44 @@
-﻿namespace PiedraPapelTijera.VistaModelo
+﻿using PiedraPapelTijera.Modelo;
+using System.Collections.ObjectModel;
+
+namespace PiedraPapelTijera.VistaModelo
 {
     public class InicioVistaModelo
     {
+        public ObservableCollection<Opcion> Opciones { get; private set; }
+        public ObservableCollection<Jugador> Jugadores { get; set; }
+
+        public InicioVistaModelo()
+        {
+            Opciones = new ObservableCollection<Opcion>
+            {
+                new Opcion
+                {
+                    Nombre = "Piedra",
+                    NombreArchivo = "piedra.png"
+                },
+                new Opcion
+                {
+                    Nombre = "Papel",
+                    NombreArchivo = "papel.png"
+                },
+                new Opcion
+                {
+                    Nombre = "Tijera",
+                    NombreArchivo = "tijera.png"
+                }
+            };
+            Jugadores = new ObservableCollection<Jugador>
+            {
+                new Jugador
+                {
+                     EsHumano = true
+                },
+                new Jugador
+                {
+                    EsHumano = false
+                }
+            };
+        }
     }
 }
